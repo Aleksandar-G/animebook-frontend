6c2695abe581085b9738c3f27e856c229b434c92
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-03-17 10:41:26 +0100

    feat(Navbar.js): add navbar component

---
+++ globalComponents/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
