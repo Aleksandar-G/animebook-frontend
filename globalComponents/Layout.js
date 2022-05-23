011dccb1ea0a6049d4b98d0a6a830139c0d598be
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-05-23 12:51:23 +0200

    feat(Layout.js): add globalCOmponent for layout

---
+++ globalComponents/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export const Layout = () => {
+  return (
+    <Head>
+      <title>Animebook</title>
+      <meta
+        name="description"
+        content="social media for people who enjoy anime"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
