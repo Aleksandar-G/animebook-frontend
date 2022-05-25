cd236238b53f70958750efb398054c38ed4e3b65
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-05-25 17:10:48 +0200

    feat(Navabr.js): logout functionality

--- globalComponents/Navbar.js
+++ globalComponents/Navbar.js
@@ -1,23 +1,37 @@
 import Link from "next/link";
+import { removeCookies } from "cookies-next";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link href="/">
-                <a className="navbar-brand">Navbar</a>
-            </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <Link href="/">
-                        <a className="nav-item nav-link active" href="#">Home</a>
-                    </Link>
-                </div>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link href="/">
+        <a className="navbar-brand">Navbar</a>
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav"></div>
+      </div>
+      <a
+        className="nav-item nav-link active float-end"
+        onClick={() => {
+          removeCookies("token");
+          window.location.href = "/";
+        }}
+      >
+        Logout
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
